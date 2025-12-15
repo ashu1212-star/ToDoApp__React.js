26b79ac63322bde6b678c5e027ab98693f55a38a
ashu <ashutosh.pal26@gmail.com>
2025-12-16 00:50:25 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -10,3 +10,31 @@ function App() {
 }
 
 export default App;
+
+// sonar test line 1
+// sonar test line 2
+// sonar test line 3
+// sonar test line 4
+// sonar test line 5      
+// sonar test line 6
+// sonar test line 7
+// sonar test line 8
+// sonar test line 9
+// sonar test line 10
+// sonar test line 11
+// sonar test line 12
+// sonar test line 13
+// sonar test line 14
+// sonar test line 15
+// sonar test line 16
+// sonar test line 17
+// sonar test line 18
+// sonar test line 19
+// sonar test line 20
+// sonar test line 21
+// sonar test line 22
+// sonar test line 23
+// sonar test line 24
+// ...
+// sonar test line 25
+
